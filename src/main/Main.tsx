2eaba210987cc2bb523b07a69da38cd71a2e9c2f
Camila Paschini <camila.paschini@gmail.com>
2025-06-12 12:22:51 +0200

    cleanup and added some plans

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -20,8 +20,6 @@ const useStyles = createUseStyles((theme: ThemeProps) => ({
     width: '100%',
     height: '96%',
     margin: '20px 0 0 0',
-    alignContent: 'center',
-    justifyContent: 'staart',
     padding: '0 40px 0 40px'
   },
   notificationArea: {
@@ -46,6 +44,14 @@ const Main = () => {
   const { data, dataIsLoading, dataError, metadata } = useData(endpoint);
   const [isShowingNotification, setIsShowingNotification] = useState(true);
   const [activeView, setActiveView] = useState<'list' | 'grid' | 'details'>('list');
+
+  // TODO: activeView and errors need to be a global context
+  // TODO: product icons are probably images that can be extracted from the magic image url
+  // Image urls can be built using
+  // https://images.svc.ui.com/?u=https%3A%2F%2Fstatic.ui.com%2Ffingerprint%2Fui%2Fimages%2F${id}%2Fdefault%2F${images.default}.png&w=${size}&q=75
+
+  // For an example see
+  // https://images.svc.ui.com/?u=https%3A%2F%2Fstatic.ui.com%2Ffingerprint%2Fui%2Fimages%2Fed67d43e-2d5c-4928-ace8-edf984baeff1%2Fdefault%2F977c1f8c477549aeb7238727fd4ecc62.png&w=640&q=75
 
   data && console.log(data.devices);
 
